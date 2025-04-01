d2c7c758837408ff009b10774410bd02c2f80a7b
Artem Kargopolov <kur4g1n@gmail.com>
2025-04-02 02:05:52 +0300

    Updated parser tests

--- tests/unit_tests/test_parser.py
+++ tests/unit_tests/test_parser.py
@@ -1,13 +1,18 @@
 import operator
 import pytest
 
+from cli_calculator.models.errors import ComplexConstantError
 from cli_calculator.models.expression import (
     BinaryOperatorNode,
     ConstantNode,
     ExpressionNode,
     UnaryOperatorNode,
 )
-from cli_calculator.parsing.errors import ExpressionFormatError, UnsupportedLexemeError, UnsupportedNodeTypeError
+from cli_calculator.parsing.errors import (
+    ExpressionFormatError,
+    UnsupportedLexemeError,
+    UnsupportedNodeTypeError,
+)
 from cli_calculator.parsing.parser import ExpressionParser
 
 
@@ -16,28 +21,33 @@ from cli_calculator.parsing.parser import ExpressionParser
     [
         ("2 + 2", BinaryOperatorNode(operator.add, ConstantNode(2), ConstantNode(2))),
         (
-            "1234.15 / 1000.5 * (-13 - 100)",
-            BinaryOperatorNode(
-                operator.mul,
-                BinaryOperatorNode(
-                    operator.truediv, ConstantNode(1234.15), ConstantNode(1000.5)
-                ),
-                BinaryOperatorNode(
-                    operator.sub,
-                    UnaryOperatorNode(operator.neg, ConstantNode(13)),
-                    ConstantNode(100),
-                ),
-            ),
+            "22.45 - 23.89",
+            BinaryOperatorNode(operator.sub, ConstantNode(22.45), ConstantNode(23.89)),
         ),
         (
-            "-(-134.12*-13)",
-            UnaryOperatorNode(
-                operator.neg,
+            "331.96 * 112.7",
+            BinaryOperatorNode(operator.mul, ConstantNode(331.96), ConstantNode(112.7)),
+        ),
+        (
+            "1379 / 9782",
+            BinaryOperatorNode(operator.truediv, ConstantNode(1379), ConstantNode(9782)),
+        ),
+        (
+            "-786",
+            UnaryOperatorNode(operator.neg, ConstantNode(786)),
+        ),
+        (
+            "1234.15 / 1000.5 * -13 - 100",
+            BinaryOperatorNode(
+                operator.sub,
                 BinaryOperatorNode(
                     operator.mul,
-                    UnaryOperatorNode(operator.neg, ConstantNode(134.12)),
+                    BinaryOperatorNode(
+                        operator.truediv, ConstantNode(1234.15), ConstantNode(1000.5)
+                    ),
                     UnaryOperatorNode(operator.neg, ConstantNode(13)),
                 ),
+                ConstantNode(100),
             ),
         ),
     ],
@@ -46,15 +56,18 @@ def test_parser_general(expression: str, tree: ExpressionNode):
     root = ExpressionParser.parse_string(expression)
     assert root == tree
 
-@pytest.mark.parametrize("expression,error,message",
+
+@pytest.mark.parametrize(
+    "expression,error,message",
     [
         ("3 ! 3", ExpressionFormatError, "Wrong expression format. Unable to proceed to parsing"),
         ("2 + 2 ; -1", ExpressionFormatError, "Multiple or no expressions are unsupported"),
         ("", ExpressionFormatError, "Multiple or no expressions are unsupported"),
         ("2 & 2", UnsupportedLexemeError, "Binary operator BitAnd is not supported"),
         ("+2", UnsupportedLexemeError, "Unary operator UAdd is not supported"),
-        ("await 3", UnsupportedNodeTypeError, "Unsupported node type: Await")
-    ]
+        ("await 3", UnsupportedNodeTypeError, "Unsupported node type: Await"),
+        ("1 + 4j", ComplexConstantError, "Value 4j of type complex is not supported"),
+    ],
 )
 def test_parser_errors(expression: str, error: type[Exception], message: str):
     with pytest.raises(error) as e_info:
